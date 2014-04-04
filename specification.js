e765c1a89b9dbba9f03f539f4f7291310ca4888d
Anton Muraviev <chromice@gmail.com>
2014-04-04 15:34:55 +0100

    Add more font families and improve tests.

--- specification.js
+++ specification.js
@@ -1,11 +1,15 @@
-// Calculate baseline offsets for common font families
 (function () {
-	var fontFamilies = document.querySelectorAll('#baseline-offset-table tbody th'),
-		testScale = document.getElementById('scale-test'),
-		testScaleSteps = document.querySelectorAll('#scale-test span');
+	// Based on http://stackoverflow.com/a/10073788/341041
+	function pad_right(n, width, z) {
+		z = z || '0';
+		n = n + '';
+		return n.length >= width ? n : n + new Array(width - n.length + 1).join(z);
+	}
 	
-	[].forEach.call(fontFamilies, function(th, i){
-		var fontFamily = th.textContent,
+	// Baseline offset value
+	function font_offset_value(fontFamily) {
+		var testScale = document.getElementById('scale-test'),
+			testScaleSteps = document.querySelectorAll('#scale-test span'),
 			candidateSum = 0,
 			weightSum = 0,
 			firstHeight = 0,
@@ -36,6 +40,27 @@
 			firstOffset = offset;
 		});
 		
-		th.nextElementSibling.textContent = Math.round(1000 * candidateSum / weightSum) / 1000;
+		return pad_right(Math.round(1000 * candidateSum / weightSum) / 1000, 5);
+	}
+	
+	// Calculate baseline offset values for in all tables
+	var fallbackFont = 'Comic Sans MS',
+		fallbackOffset = font_offset_value(fallbackFont);
+	
+	[].forEach.call(document.querySelectorAll('.offset-table tbody th'), function(th, i){
+		var fontFamily = '"' + th.textContent + '", "' + fallbackFont + '"',
+			fontOffset = font_offset_value(fontFamily);
+		
+		th.style.fontFamily = fontFamily;
+		
+		if (fontOffset !== fallbackOffset || th.textContent === fallbackFont) {
+			th.nextElementSibling.textContent = fontOffset;
+		}
+		
+		// Highlight rows that do not meet expectations
+		if (th.nextElementSibling.textContent !== th.nextElementSibling.nextElementSibling.textContent) {
+			th.parentNode.className += ' error';
+		}
+		
 	});
 })();
